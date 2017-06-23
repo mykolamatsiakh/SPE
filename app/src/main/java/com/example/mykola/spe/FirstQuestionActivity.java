6621953110831c8c4e636d8480f688500f45fc5f
mykolamatsiakh <mmatsiakh@gmail.com>
2017-06-23 20:23:09 +0300

    [+] add all layouts to project

---
+++ app/src/main/java/com/example/mykola/spe/FirstQuestionActivity.java
@@ -0,0 +1,16 @@
+package com.example.mykola.spe;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by mykola on 22.06.17.
+ */
+
+public class FirstQuestionActivity {
+
+}
